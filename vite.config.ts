9e112f177be6338db9cebe7bb8ff30cd6c9ae9bf
Minseo <alstj6009@gmail.com>
2025-04-26 01:20:16 +0900

    의존성 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-     external: ['react-bootstrap', 'react-router-dom', 'react-icons'], 
+     external: ['react-bootstrap', 'react-router-dom', 'react-icons/fa'], 
     },
   },
 });
