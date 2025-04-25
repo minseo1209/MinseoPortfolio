c8f8194c0fa47eaf431c03be114c341e45ea698d
Minseo <alstj6009@gmail.com>
2025-04-26 03:39:52 +0900

    test

--- web.js
+++ web.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// __dirname 대체
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
